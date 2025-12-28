b5b7b637c84dac86ded3c8bf9a199c5eef6ec336
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-12-28 09:51:51 +0900

    Add a script for tests

---
+++ client.mjs
@@ -0,0 +1,21 @@
+await registerTask();
+
+/**
+ * POST /api/v1/tasks
+ */
+async function registerTask() {
+    const res = await fetch("http://127.0.0.1:8080/api/v1/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            title: "Sample Task",
+            description: "This is a test task",
+            status: 1,
+        }),
+    });
+
+    const json = await res.json();
+    console.log(res.status, json);
+}
