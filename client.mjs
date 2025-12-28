8c452d583b81a7966c354777ef11ec1e6cdd5ddf
T-Ageishi <takuto.ageishi.dev@gmail.com>
2025-12-28 11:08:23 +0900

    Add a function to register multiple tasks

--- client.mjs
+++ client.mjs
@@ -1,5 +1,4 @@
-// await registerTask();
-
+await registerTasks(10);
 await listTask();
 
 /**
@@ -32,4 +31,35 @@ async function registerTask() {
 
     const json = await res.json();
     console.log(res.status, json);
+}
+
+/**
+ * register multiple tasks
+ * POST /api/v1/tasks
+ */
+async function registerTasks(count) {
+    for (let i = 0; i < count; i++) {
+        const res = await fetch("http://127.0.0.1:8080/api/v1/tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTask(i)),
+        });
+
+        const json = await res.json();
+        console.log(res.status, json);
+    }
+
+    function newTask(i) {
+        const min = 1;
+        const max = 3;
+        const status = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        return {
+            title: `Sample Task${i === 0 ? "" : `(${i})`}`,
+            description: `This is a test task${i === 0 ? "" : `(${i})`}`,
+            status,
+        };
+    }
 }
